20f4b2fe7f61947ed158e727d3ada268284f0323
medad21 <rangimedad488@gmail.com>
2025-11-19 21:04:37 +0330

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def analyze():
         "previous_price": prev
     })
 
+app.run()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+
+
