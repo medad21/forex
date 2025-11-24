6fb7f39d0674396e5d0e8b5ebdc69f7c72852ad1
medad21 <rangimedad488@gmail.com>
2025-11-24 22:45:57 +0330

    Update train.py

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from xgboost import XGBClassifier
 
 # ✅ لیست کامل نمادها برای آموزش ترکیبی (5 نماد اصلی)
 SYMBOLS = ["EURUSD=X", "GBPUSD=X", "USDJPY=X", "GC=F", "BTC-USD"]
-PERIOD = "2y"   # افزایش دوره به 3 سال برای داده‌های بیشتر
+PERIOD = "1y"   # افزایش دوره به 3 سال برای داده‌های بیشتر
 INTERVAL = "1h" # تایم فریم 1 ساعته
 
 # --- توابع کمکی ---
